137c5027dbe4a8adde2b35c0822247e1b970c6a9
Andrei Gravonski <gravonskia@gmail.com>
2025-01-19 21:58:27 -0300

    Dados Primitivos em Java

---
+++ JavaNinja/src/TiposDeDados/DadosPrimitivos.java
@@ -0,0 +1,12 @@
+package TiposDeDados;
+
+public class DadosPrimitivos {
+    public static void main(String[] args) {
+        /*Objetivo: Criar um Ninja Naruto com dados primitivos*/
+        int idade = 16;
+        double altura = 1.68;
+        char inicial = 'N';
+        boolean vivoOuMorto = true;
+        Long saldoBancario = 999999999L;
+    }
+}

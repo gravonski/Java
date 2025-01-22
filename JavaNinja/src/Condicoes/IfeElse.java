0bb22ffb374862ed977536c587251cd9253c654e
Andrei Gravonski <gravonskia@gmail.com>
2025-01-21 22:27:37 -0300

    Exercícios com IF & ELSE

---
+++ JavaNinja/src/Condicoes/IfeElse.java
@@ -0,0 +1,23 @@
+package Condicoes;
+
+public class IfeElse {
+    public static void main(String[] args) {
+        /*
+        * Passar o NINJA de nível conforme o número de missões realizadas;
+        * */
+
+        String nome = "Naruto Uzumaki";
+        int idade = 16;
+        boolean hokage = false;
+        short numeroDeMissoes = 20;
+        String rank;
+
+        if (numeroDeMissoes >= 10 && numeroDeMissoes <= 19 && idade >= 15) {
+            System.out.println("Rank: Chunnin");
+        } else if (numeroDeMissoes >= 20) {
+            System.out.println("Rank: Jounin");
+        } else {
+            System.out.println("Rank: Gennim");
+        }
+    }
+}
--- JavaNinja/src/TiposDeDados/DadosNaoPrimitivos.java
+++ JavaNinja/src/TiposDeDados/DadosNaoPrimitivos.java
@@ -2,6 +2,16 @@ package TiposDeDados;
 
 public class DadosNaoPrimitivos {
     public static void main(String[] args) {
+        /*
+        * Objetivo: Criar um NINJA e atribuir métodos a ele;
+        * */
+        String name = "Naruto Uzumaki";
+        String nameUpperCase = name.toUpperCase();
+        System.out.println("Eu sou o " + name);
+        System.out.println(nameUpperCase + " é meu nome");
 
+        String village = "Aldeia da Folha";
+        String villageLowerCase = village.toLowerCase();
+        System.out.println(villageLowerCase);
     }
 }

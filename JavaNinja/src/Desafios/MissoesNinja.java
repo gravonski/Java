21d1db3d2d0da81b8edc931f862a4ddceb1ec515
Andrei Gravonski <gravonskia@gmail.com>
2025-01-26 21:59:07 -0300

    Finalização desafio MissoesNinja

--- JavaNinja/src/Desafios/MissoesNinja.java
+++ JavaNinja/src/Desafios/MissoesNinja.java
@@ -1,84 +1,81 @@
 package Desafios;
 
+import jdk.jshell.Snippet;
+
 public class MissoesNinja {
     public static void main(String[] args) {
         /*Ninja 1*/
         String ninja1 = "Naruto";
         int idade1 = 13;
         String missao1 = "Escoltar Tazuna";
-        char dificuldade1 = 'A';
-        boolean concluido1 = false;
+        char dificuldade1 = 'd';
+        String status1 = "Conclúida";
+
 
         /*Ninja 2*/
         String ninja2 = "Sasuke";
-        int idade2 = 14;
+        int idade2 = 15;
         String missao2 = "Exame Chūnin";
         char dificuldade2 = 'B';
-        boolean concluido2 = true;
+        String status2 = "Em Andamento";
 
         /*Ninja 3*/
         String ninja3 = "Sakura";
-        int idade3 = 15;
+        int idade3 = 12;
         String missao3 = "Resgatar Sasuke";
         char dificuldade3 = 'A';
-        boolean concluido3 = true;
+        String status3 = "Iniciando";
 
-        String mensagem1;
-        String mensagem2;
-        String mensagem3;
-
-        if (concluido1){
-            mensagem1 = "Missão conclúida";
+        if (idade1 < 15) {
+            if(dificuldade1 == 'C' || dificuldade1 == 'D' || dificuldade1 == 'c' || dificuldade1 == 'd') {
+                status1 = "Missão Conclúida";
+            } else {
+                status1 = "O Ninja não tem idade suficiente para concluir a missão";
+            }
         } else {
-            mensagem1 = "Missão não concluída";
-        }
-
-        if (concluido2){
-            mensagem2 = "Missão conclúida";
-        } else {
-            mensagem2 = "Missão não concluída";
-        }
-
-        if (concluido3) {
-            mensagem3 = "Missão conclúida";
-        } else {
-            mensagem3 = "Missão não concluída";
+            status1 = "Missão concluída com sucesso";
         }
 
         if (idade1 < 15) {
-            if (dificuldade1 == 'C') {
-                System.out.println(mensagem1 + " pelo Ninja " + ninja1);
-            } else if (dificuldade1 == 'D') {
-                System.out.println(mensagem1 + " pelo Ninja " + ninja1);
+            if(dificuldade2 == 'C' || dificuldade2 == 'D' || dificuldade2== 'c' || dificuldade2 == 'd') {
+                status2 = "Missão Conclúida";
             } else {
-                System.out.println(mensagem1 + " pelo Ninja " + ninja1);
+                status2 = "O Ninja não tem idade suficiente para concluir a missão";
             }
         } else {
-            System.out.println(mensagem1 + " pelo Ninja " + ninja1);
+            status2 = "Missão concluída com sucesso";
         }
 
-        if (idade2 < 15) {
-            if (dificuldade2 == 'C') {
-                System.out.println(mensagem2 + " pelo Ninja " + ninja2);
-            } else if (dificuldade2 == 'D') {
-                System.out.println(mensagem2 + " pelo Ninja " + ninja2);
+        if (idade1 < 15) {
+            if(dificuldade3 == 'C' || dificuldade3 == 'D' || dificuldade3 == 'c' || dificuldade3 == 'd') {
+                status3 = "Missão Conclúida";
             } else {
-                System.out.println(mensagem2 + " pelo Ninja " + ninja2);
+                status3 = "O Ninja não tem idade suficiente para concluir a missão";
             }
         } else {
-            System.out.println(mensagem2 + " pelo Ninja " + ninja2);
+            status3 = "Missão concluída com sucesso";
         }
 
-        if (idade3 < 15) {
-            if (dificuldade3 == 'C') {
-                System.out.println(mensagem3 + " pelo Ninja " + ninja3);
-            } else if (dificuldade3 == 'D') {
-                System.out.println(mensagem3 + " pelo Ninja " + ninja3);
-            } else {
-                System.out.println(mensagem3 + " pelo Ninja " + ninja3);
-            }
-        } else {
-            System.out.println(mensagem3 + " pelo Ninja " + ninja3);
-        }
+        System.out.println("-----------------------------------------");
+        System.out.println("Nome do Ninja: " + ninja1);
+        System.out.println("Idade do Ninja: " + idade1);
+        System.out.println("Missão: " + missao1);
+        System.out.println("Dificuldade da Missão: " + dificuldade1);
+        System.out.println("Status da Missão: " + status1);
+        System.out.println("-----------------------------------------");
+
+        System.out.println("Nome do Ninja: " + ninja2);
+        System.out.println("Idade do Ninja: " + idade2);
+        System.out.println("Missão: " + missao2);
+        System.out.println("Dificuldade da Missão: " + dificuldade2);
+        System.out.println("Status da Missão: " + status2);
+
+        System.out.println("-----------------------------------------");
+        System.out.println("Nome do Ninja: " + ninja3);
+        System.out.println("Idade do Ninja: " + idade3);
+        System.out.println("Missão: " + missao3);
+        System.out.println("Dificuldade da Missão: " + dificuldade3);
+        System.out.println("Status da Missão: " + status3);
+        System.out.println("-----------------------------------------");
     }
 }

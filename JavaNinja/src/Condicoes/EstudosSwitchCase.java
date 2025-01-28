7a8b48ea86ab7053ffe7a50795183ccba1cfa96f
Andrei Gravonski <gravonskia@gmail.com>
2025-01-28 18:05:46 -0300

    ternarios e switch case

---
+++ JavaNinja/src/Condicoes/EstudosSwitchCase.java
@@ -0,0 +1,32 @@
+package Condicoes;
+
+import java.util.Scanner;
+
+public class EstudosSwitchCase {
+    public static void main(String[] args) {
+        /*Swich Case: Servem para gerar casos ESPECÍFICOS
+        */
+        Scanner caixaDeTexto = new Scanner(System.in);
+        System.out.println("Escolha um personagem:");
+        System.out.println("1 - Naruto Uzumaki");
+        System.out.println("2 - Sakura Haruno");
+        System.out.println("3 - Sasuke Uchiha");
+
+        int choose = caixaDeTexto.nextInt();
+
+        switch (choose) {
+            case 1:
+                System.out.println("Você escolheu o Naruto Uzumaki");
+                break;
+            case 2:
+                System.out.println("Você escolheu a Sakura Haruno");
+                break;
+            case 3:
+                System.out.println("Você escolheu o Sasuke Uchiha");
+                break;
+            default:
+                System.out.println("Resposta inválida! Tente novamente");
+        }
+        caixaDeTexto.close();
+    }
+}
---
+++ JavaNinja/src/Condicoes/Ternarios.java
@@ -0,0 +1,16 @@
+package Condicoes;
+
+public class Ternarios {
+    public static void main(String[] args) {
+        /*
+        *Ternários : maneira de reduzir o código
+        * */
+
+        int idade = 10;
+
+        String age = (idade <= 10) ? "O ninja tem 10 anos ou menos" : "Idade maior que 10";
+
+        System.out.println("Idade: " + idade);
+        System.out.println(age);
+    }
+}

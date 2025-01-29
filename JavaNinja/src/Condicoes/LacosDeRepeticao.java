df700ccfddc4abc3d83f9399d93d667c55ecf78f
Andrei Gravonski <gravonskia@gmail.com>
2025-01-28 22:16:44 -0300

    Laços de Repetição

---
+++ JavaNinja/src/Condicoes/LacosDeRepeticao.java
@@ -0,0 +1,18 @@
+package Condicoes;
+
+public class LacosDeRepeticao {
+    public static void main(String[] args) {
+        /*Laços de Repetição: Vão ser executados infinitamente ou até o parâmetro ser executado.*/
+        int numeroDeClones = 0;
+        int numeroMaxClones = 40;
+
+       /* while (numeroDeClones <= numeroMaxClones) {
+            numeroDeClones++;
+            System.out.println("Clone n°" + numeroDeClones + " do Naruto foi criado");
+        }
+*/
+        for (int i = 0; i < numeroMaxClones; i++) {
+            System.out.println("Clone n°" + i + " do Naruto foi criado");
+        }
+    }
+}

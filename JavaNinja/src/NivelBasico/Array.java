ff74f8179ee822628b4a8069d79f49a3b4d85de9
Andrei Gravonski <gravonskia@gmail.com>
2025-01-29 22:39:05 -0300

    Arrays em JAVA

---
+++ JavaNinja/src/NivelBasico/Array.java
@@ -0,0 +1,21 @@
+package NivelBasico;
+
+public class Array {
+    public static void main(String[] args) {
+
+        String[] ninja = new String[5];
+        ninja[0] = "Naruto Uzimaki";
+        ninja[1] = "Sakura Haruno";
+        ninja[2] = "Sasuke Uchiha";
+        ninja[3] = "Hinata Hyuga";
+        ninja[4] = "Rock Lee";
+        System.out.println(ninja);
+
+        int[] idade = new int[5];
+        idade[0] = 15;
+        idade[1] = 14;
+        idade[2] = 16;
+        idade[3] = 16;
+        idade[4] = 12;
+    }
+}

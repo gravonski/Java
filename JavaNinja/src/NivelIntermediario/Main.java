f41cbeed91a05011f74a61fae02fd02e036c968f
Andrei Gravonski <gravonskia@gmail.com>
2025-02-19 23:47:08 -0300

    Stacks e Queues

--- JavaNinja/src/NivelIntermediario/Main.java
+++ JavaNinja/src/NivelIntermediario/Main.java
@@ -2,8 +2,7 @@ package NivelIntermediario;
 
 import NivelBasico.Arrays.Array;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -21,7 +20,26 @@ public class Main {
         ninjasList.add("Sasuke Uchiha");
         ninjasList.add("Sakura Haruno");
         System.out.println(ninjasList);
+        //Stack
+        System.out.println("---------------STACK-----------------");
+        Stack <String> ninjasStack = new Stack<>();
+        ninjasStack.push("Sasuke Uchiha");
+        ninjasStack.push("Naruto Uzumaki");
+        ninjasStack.push("Sakura Haruno");
+        System.out.println("Stack completa: " + ninjasStack);
+        ninjasStack.pop();
+        System.out.println("Stack com Pop: " + ninjasStack);
+        System.out.println("Peek da Stack: " + ninjasStack.peek());
 
-
+        System.out.println("---------------QUEUE-----------------");
+        Queue <String> ninjasQueue = new LinkedList<>();
+        ninjasQueue.add("Naruto Uzumaki");
+        ninjasQueue.add("Sasuke Uchiha");
+        ninjasQueue.add("Sakura Haruno");
+        ninjasQueue.add("Kakashi Uchiha");
+        System.out.println("Ninjas na fila: " + ninjasQueue);
+        ninjasQueue.poll();
+        System.out.println("Fila com Pool: " + ninjasQueue);
+        System.out.println("Topo da Fila: " + ninjasQueue.peek());
     }
 }

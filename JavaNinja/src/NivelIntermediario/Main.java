8df5667ab33a0d849f3b53eb6ea2750d134b2394
Andrei Gravonski <gravonskia@gmail.com>
2025-02-23 22:08:39 -0300

    SetList

--- JavaNinja/src/NivelIntermediario/Main.java
+++ JavaNinja/src/NivelIntermediario/Main.java
@@ -1,7 +1,23 @@
 package NivelIntermediario;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
+    public static void main(String[] args) {
 
+        List <String> listaNinja = new ArrayList<>();
+        listaNinja.add("Naruto Uzumaki");
+        listaNinja.add("Sasuke Uchiha");
+        listaNinja.add("Sakura Haruno");
+        listaNinja.add("Naruto Uzumaki");
+
+        Set <String> setNinjas = new HashSet<>();
+        setNinjas.addAll(listaNinja);
+
+
+        System.out.println(setNinjas);
+    }
 }
